15676652218cdebe96c0f931c873d6715046daae
Loomeh <zanehj10@gmail.com>
2022-12-25 18:29:53 +0000

    first commit

---
+++ .dotfiles/res.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --rate 75 --output DVI-D-0 --off --output DVI-D-1 --mode 1280x1024 --pos 1920x0 --rotate normal --rate 75
